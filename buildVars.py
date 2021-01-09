3a9c0e7a56d73cb60248a4d4e57a6116446a2650
paulber19 <paulber19@laposte.net>
2021-01-09 08:03:17 +0100

    version 2.55.1

--- buildVars.py
+++ buildVars.py
@@ -44,7 +44,7 @@ Compatible with VLC 3.0.
 """),
 
 	# version
-	"addon_version": "2.5",
+	"addon_version": "2.5.1",
 	# Author(s)
 	"addon_author": "PaulBer19",
 	# URL for the add-on documentation support
